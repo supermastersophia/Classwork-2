800899d1bed73a1a3eb7bef6405c9c8730392c0e
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 15:36:38 -0700

    Create Login.py

---
+++ Input/Login.py
@@ -0,0 +1,23 @@
+#File name: Login.py
+#Author: Sophia A
+#Date created: February 25, 2025
+#Description: Log in to your account
+
+email_username = input("Email or Username: ")
+password = input("Password: ")
+print(" ")
+print(" ")
+
+print("Login?")
+print(" ")
+print("A) Yes")
+print("B) No")
+
+print(" ")
+answer = input(" ")
+
+if answer == 'A':
+    print("\n")
+    print("Access GRANTED")
+else:
+    exit()
