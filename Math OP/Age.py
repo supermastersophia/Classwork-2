4e19cf388f3b8792903910058cc22efa780ff11d
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 15:48:52 -0700

    Create Age.py

---
+++ Math OP/Age.py
@@ -0,0 +1,16 @@
+#File name: Age.py
+#Author: Sophia A
+#Date created: Feburary 4, 2025
+#Description: This is my age in weeks
+
+question = int(input("Enter your current age: "))
+print(" ")
+print("This is your age in weeks:")
+
+age = ((question*365)+(question//4))
+int(age)
+weeks = age//7
+int(weeks)
+final_age = (age%7) + weeks
+
+print(final_age)
