772dc814c2f6b33c3835bbf0b828a9b53a472323
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 16:51:43 -0700

    Create Olympics.py

---
+++ Turtle/Olympics.py
@@ -0,0 +1,37 @@
+#File name: Olympics.py
+#Author: Sophia A
+#Date created: March 25, 2025
+#Description: This is the Olympics logo
+
+import turtle
+square = turtle.Turtle()
+
+square.color("#317cf7")
+square.penup()
+square.goto(-120, 0)
+square.pendown()
+square.circle(50)
+
+square.color("#000000")
+square.penup()
+square.goto(0, 0)
+square.pendown()
+square.circle(50)
+
+square.color("#ff0000")
+square.penup()
+square.goto(120, 0)
+square.pendown()
+square.circle(50)
+
+square.color("#ffd41e")
+square.penup()
+square.goto(-60, -60)
+square.pendown()
+square.circle(50)
+
+square.color("#008000")
+square.penup()
+square.goto(60, -60)
+square.pendown()
+square.circle(50)
