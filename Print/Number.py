8e11001bd83923a95f546f5a087e419488204dee
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 15:53:43 -0700

    Create Number.py

---
+++ Print/Number.py
@@ -0,0 +1,12 @@
+#File name: Number.py
+#Author: Sophia A
+#Date created: January 23, 2025
+#Description: These are two number pyramids
+
+pyramid1 ="1                                                                     2 2                                                                   3 3 3                                                                 4 4 4 4                                                               5 5 5 5 5"
+print(" ")
+print(pyramid1)
+
+print(" ")
+pyramid2="5 4 3 2 1                                                             4 3 2 1                                                               3 2 1                                                                 2 1                                                                   1"
+print(pyramid2)
