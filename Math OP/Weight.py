e9fe1d35ec190e420e22cc8612d316d063d07872
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 15:49:38 -0700

    Create Weight.py

---
+++ Math OP/Weight.py
@@ -0,0 +1,12 @@
+#File name: Weight.py
+#Author: Sophia A
+#Date created: Feburary 4, 2025
+#Description: This is your weight on the moon
+
+print("This is your weight on the moon:")
+print(" ")
+weight=int(input("Enter your current weight in pounds: "))
+moon=weight//6
+
+print(" ")
+print("Your weight (on the moon) is approximately", moon , "pounds.")
