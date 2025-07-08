fe84f59dc5221e1fdbe7282063d0ba4e00c40f9b
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 15:38:32 -0700

    Create Money.py

---
+++ Input/Money.py
@@ -0,0 +1,12 @@
+#File name: Money.py
+#Author: Sophia A
+#Date created: March 4, 2025 (National Grammar Day)
+#Description: This converts nickels into dollars
+
+print("This machine converts nickels into dollars.")
+print(" ")
+
+nickels = (int)(input("Enter a whole number: "))
+dollars = nickels/20
+print(" ")
+print(dollars)
