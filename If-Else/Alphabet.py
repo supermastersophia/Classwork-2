cd492af0e8b162270ca5b695b37d58adb9336671
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 15:25:30 -0700

    Create Alphabet.py

---
+++ If-Else/Alphabet.py
@@ -0,0 +1,13 @@
+#File name: Alphabet.py
+#Author: Sophia A
+#Date created: March 11, 2025
+#Description: Checks if the letter is a vowel or consanant.
+
+letter = input("Enter a random letter: ")
+print(" ")
+
+vowels = ["a", "e", "i", "o", "u"]
+if letter.lower() in vowels:
+    print("The letter is a vowel")
+else:
+    print("The letter is a consanant")
