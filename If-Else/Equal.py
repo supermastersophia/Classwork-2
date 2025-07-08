7273341ba147bb6ef63e60284bfc5e02370c7f02
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 15:33:15 -0700

    Create Equal.py

---
+++ If-Else/Equal.py
@@ -0,0 +1,17 @@
+#File name: Equal.py
+#Author: Sophia A
+#Date created: March 7, 2025
+#Description: Determines equal numbers
+
+number1 = (int)(input("Enter a number: "))
+number2 = (int)(input("Enter another number: "))
+print(" ")
+print(" ")
+print("The two numbers are...")
+
+if number1 == number2:
+    print(" ")
+    print("Equal")   
+else:
+    print(" ")
+    print("Not equal.")
