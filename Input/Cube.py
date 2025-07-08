d8adec0cd6c3f90e66f3a56136f6babd3ada0f42
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 15:35:04 -0700

    Create Cube.py

---
+++ Input/Cube.py
@@ -0,0 +1,11 @@
+#File name: Cube.py
+#Author: Sophia A
+#Date created: February 28, 2025
+#Description: The cube of a number
+
+number = (int)(input("Enter a random number: "))
+print(" ")
+cube = number*number*number
+
+print("The cube of that number is......")
+print(cube)
