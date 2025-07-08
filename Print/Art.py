949d84759fd5a3d74e176ab28c6b0986a535484f
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 15:51:11 -0700

    Create Art.py

---
+++ Print/Art.py
@@ -0,0 +1,20 @@
+#File name: Art.py
+#Author: Sophia A
+#Date created: January 16, 2025
+#Description: This is a chocolate bar and a sunset
+
+print("    ____________________________")
+print("   /___/___/___/___/___/___/___/|")
+print("  /___/___/___/___/___/___/___/ |")
+print(" |    |    |    |    |    |   | /")
+print(" |___ |___ |___ |___ |__ _|__ |/")
+
+
+print(" ")
+print(" ")
+print("          \     *     /")
+print("               ___                 ^^   ^^")
+print("             *     *")
+print("     ---   (         )   ---          ^^")
+print("~-~~^~~-~--~^~^---~^^~~~~-~-~~~~----~-~-~-~-~~^^~^~~~-~^~")
+print(". : . : . : . : . : . : . : . : . : . : . : . : . : . : .")
