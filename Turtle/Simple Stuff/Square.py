174956a48a24cd0f9a2be9ff978d3160688d474e
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 16:00:59 -0700

    Create Square.py

---
+++ Turtle/Simple Stuff/Square.py
@@ -0,0 +1,25 @@
+#File name: Square.py
+#Author: Sophia A
+#Date created: March 19, 2025
+#Description: This is a square
+
+import turtle
+square = turtle.Turtle()
+
+square.pencolor("Orange")
+square.pensize(5)
+square.color("Orange")
+square.begin_fill()
+
+square.forward(100)
+square.left(90)
+square.forward(100)
+square.left(90)
+square.forward(100)
+square.left(90)
+square.forward(100)
+square.left(90)
+
+square.penup()
+square.end_fill()
+square.pendown()
