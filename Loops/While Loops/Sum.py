cf71db30123b523734b1e0a4477a7b7f279c6e17
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 15:48:03 -0700

    Create Sum.py

---
+++ Loops/While Loops/Sum.py
@@ -0,0 +1,16 @@
+#File name: Sum.py
+#Author: Sophia
+#Date created: April 15, 2025
+#Description: Sum of random numbers
+
+number = (int)(input("Enter a number: "))
+total = 0
+
+i = 2
+while(i <= number):
+    total = total + i
+    i = i + 2
+    
+print(" ")
+print(" ")
+print("The sum of the Even #s is... ", total)
