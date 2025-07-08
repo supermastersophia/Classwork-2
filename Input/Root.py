06eb8fb2af020f3256a0bcb6395365ba8dedbed7
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 15:39:14 -0700

    Create Root.py

---
+++ Input/Root.py
@@ -0,0 +1,12 @@
+# File name: Root.py
+# Author: Sophia A
+# Date created: March 4, 2025 (National Grammar Day)
+# Description: This calculates the square root
+
+number = (int)(input("Enter a number: "))
+print(" ")
+print("The square root of that number is...")
+
+sq_root = (number**0.5)
+print(" ")
+print(sq_root)
