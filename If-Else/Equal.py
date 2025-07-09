9a4c73c4330358f712d117db2f7cd9ec4b52dc3b
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 17:03:01 -0700

    Update Equal.py

--- If-Else/Equal.py
+++ If-Else/Equal.py
@@ -1,7 +1,7 @@
-# File name: Equal.py
-# Author: Sophia A
-# Date created: March 7, 2025
-# Description: Determines equal numbers
+#File name: Equal.py
+#Author: Sophia A
+#Date created: March 7, 2025
+#Description: Determines equal numbers
 
 number1 = (int)(input("Enter a number: "))
 number2 = (int)(input("Enter another number: "))
