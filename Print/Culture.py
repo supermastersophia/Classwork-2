5a6e0d3bc2e8da54055c1098d63003c08efa7b75
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 17:08:03 -0700

    Update Culture.py

--- Print/Culture.py
+++ Print/Culture.py
@@ -1,12 +1,10 @@
-# File name: Culture.py
-# Author: Sophia A
-# Date created: January 16, 2025
-# Description: This explains Iran's food, native sport, and famous landmarks
+#File name: Culture.py
+#Author: Sophia A
+#Date created: January 16, 2025
+#Description: This explains Iran's food, native sport, and famous landmarks
 
 print("Iran is the country my class and I are doing for multicultural week.")
 print("One type of kabab is Chellow Kabab. Another type of food is Zereshk Polo.")
 print("Iran is also know for its native sport, Polo.")
 print("Polo is a sport played on horseback. It is a sport that is similar to hockey.")
-print(
-    "Some famous places to visit in Iran are the Milad Tower, the Azadi Tower, and the Tombof Hafez."
-)
+print("Some famous places to visit in Iran are the Milad Tower, the Azadi Tower, and the Tombof Hafez.")
