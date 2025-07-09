b439525918720b93dbd6713fc7095718c1d61f68
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 17:15:00 -0700

    Update Stop.py

--- Turtle/Stop.py
+++ Turtle/Stop.py
@@ -1,37 +1,36 @@
-# File name: Stop.py
-# Author: Sophia A
-# Date created: March 25, 2025
-# Description: Drawing of a STOP sign
+#File name: Stop.py
+#Author: Sophia A
+#Date created: March 25, 2025
+#Description: This is a stop sign
 
 import turtle
+square = turtle.Turtle()
+square.color("Red")
+square.begin_fill()
 
-sign = turtle.Turtle()
-sign.color("Red")
-sign.begin_fill()
+square.forward(75)
+square.left(45)
+square.forward(75)
+square.left(45)
+square.forward(75)
+square.left(45)
+square.forward(75)
+square.left(45)
+square.forward(75)
+square.left(45)
+square.forward(75)
+square.left(45)
+square.forward(75)
+square.left(45)
+square.forward(75)
+square.left(45)
 
-sign.forward(75)
-sign.left(45)
-sign.forward(75)
-sign.left(45)
-sign.forward(75)
-sign.left(45)
-sign.forward(75)
-sign.left(45)
-sign.forward(75)
-sign.left(45)
-sign.forward(75)
-sign.left(45)
-sign.forward(75)
-sign.left(45)
-sign.forward(75)
-sign.left(45)
+square.end_fill()
+square.penup()
+square.goto(-35,70)
+square.pendown()
 
-sign.end_fill()
-sign.penup()
-sign.goto(-35,70)
-sign.pendown()
-
-sign.color("White")
-sign.penup()
-sign.write("STOP", font=("Arial", 40, "Bold"))
-sign.pendown()
+square.color("White")
+square.penup()
+square.write("STOP", font=("Arial", 40, "Bold"))
+square.pendown()
