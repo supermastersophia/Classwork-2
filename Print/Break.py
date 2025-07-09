1d269026acd21eea46bace6fc7a396bd2b970283
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 17:07:36 -0700

    Update Break.py

--- Print/Break.py
+++ Print/Break.py
@@ -1,12 +1,8 @@
-# File name: WinterBreak.py
-# Author: Sophia A
-# Date created: January 16, 2025
-# Description: These are some activities I did during Winter Break
+#File name: WinterBreak.py
+#Author: Sophia A
+#Date created: January 16, 2025
+#Description: These are some activities I did during Winter Break
 
 print("During winter break, I stayed at home and didn't travel much.")
-print(
-    "I spent time with my family, practiced Math problems, and also played some of my favorite video games."
-)
-print(
-    "Also, I set up a Christmas tree that took a whole week because I was a total beginner and had zero experience..."
-)
+print("I spent time with my family, practiced Math problems, and also played some of my favorite video games.")
+print( "Also, I set up a Christmas tree that took a whole week because I was a total beginner and had zero experience...")
