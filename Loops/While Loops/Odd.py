0db4128e0c7434532f14103860c06f2bbc5ad589
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 17:04:55 -0700

    Update Odd.py

--- Loops/While Loops/Odd.py
+++ Loops/While Loops/Odd.py
@@ -1,7 +1,7 @@
-# File name: Odd.py
-# Author: Sophia A
-# Date created: April 11, 2025
-# Description: This program prints odd numbers
+#File name: Odd.py
+#Author: Sophia A
+#Date created: April 11, 2025
+#Description: This program prints odd numbers
 
 i = 59
 while (i <= 59) and (i >= 31):
