380f9618c4a4a01174c1c7eb1c9ffd14189009a8
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-08 17:04:45 -0700

    Update Even.py

--- Loops/While Loops/Even.py
+++ Loops/While Loops/Even.py
@@ -1,7 +1,7 @@
-# File name: Even.py
-# Author: Sophia A
-# Date created: April 11, 2025
-# Description: This program prints even numbers
+#File name: Even.py
+#Author: Sophia A
+#Date created: April 11, 2025
+#Description: This program prints even numbers
 
 i = 10
 while i <= 20:
